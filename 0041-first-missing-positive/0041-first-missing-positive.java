0889d78eb8f0c05a2f1e1e2d5dee49b5d8f313e6
Mohammed Hashim <55325133+mohammedhashim790@users.noreply.github.com>
2025-10-31 17:52:46 -0300

    Time: 27 ms (5.18%), Space: 78.6 MB (5.57%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,35 +1,49 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
 
-        int res = 0;
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
+        // int res = 0;
+        // int min = Integer.MAX_VALUE;
+        // int max = Integer.MIN_VALUE;
 
-        HashMap<Integer, Integer> map = new HashMap();
-        for(int num: nums){
-            if(num < min && num>0) min = num;
-            if(num > max) max = num;
+        // HashMap<Integer, Integer> map = new HashMap();
+        // for(int num: nums){
+        //     if(num < min && num>0) min = num;
+        //     if(num > max) max = num;
 
-            if(num>0) {
-                res^=num;
-                map.put(num, 1);
-            }
-        }
-        if(max <=0) return 1;
+        //     if(num>0) {
+        //         res^=num;
+        //         map.put(num, 1);
+        //     }
+        // }
+        // if(max <=0) return 1;
 
-        for(int i = 1;i<=max;i++){
-            if(map.getOrDefault(i,-1) == -1) return i;
-        }
-
-        return max+1;
-        // System.out.println(min + " \t "  + max);
-        // for(;min<=max;min++){
-        //     res^=min;
+        // for(int i = max;i>0;i--){
+        //     res^=i;
         // }
 
         // System.out.println(res);
 
-        // return res != 0? res : max+1;
-           
+        int n = nums.length;
+
+        for(int i = 0;i<n;i++){
+            if(nums[i]<=0 || nums[i]>n){
+                nums[i] = n +1;
+            }
+        }
+
+        for(int i = 0;i<n;i++){
+            int num = Math.abs(nums[i]);
+            if(num <= n){
+                nums[num - 1] = Math.abs(nums[num - 1]) * -1;
+            }
+        }
+
+        System.out.println(Arrays.toString(nums));
+
+        for(int i = 0;i<n;i++){
+            if(nums[i] > 0) return i+1;
+        }
+
+        return n+1;
     }
 }
