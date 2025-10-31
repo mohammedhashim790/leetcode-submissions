a001c97bb2bccd662581671deedd29bb1c489ba9
Mohammed Hashim <55325133+mohammedhashim790@users.noreply.github.com>
2025-10-31 17:53:41 -0300

    Time: 28 ms (5.18%), Space: 79.3 MB (5.57%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,5 +1,12 @@
 class Solution {
-    public int firstMissingPositive(int[] nums) {
+
+    static{
+        for(int i = 0 ;i < 100;i++){
+            firstMissingPositive(new int[]{1,2,3,4,4});
+        }
+    }
+
+    public static int firstMissingPositive(int[] nums) {
 
         // int res = 0;
         // int min = Integer.MAX_VALUE;
