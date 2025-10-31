25bf0c146a5cf3a307607091e1802bf0665f10e3
Mohammed Hashim <55325133+mohammedhashim790@users.noreply.github.com>
2025-10-31 12:21:35 -0300

    Time: 1 ms (100%), Space: 45.6 MB (7.71%) - LeetHub

--- 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.java
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.java
@@ -1,16 +1,31 @@
 class Solution {
     public int[] getSneakyNumbers(int[] nums) {
-        int length = nums.length;
-        int[] res = new int[2];
-        int[] map = new int[length - 2];
-        int k = 0;
+        int n = nums.length - 2;
+
+        int expected = 0;
+        int actual = 0;
         for(int num: nums){
-            if(map[num] == 1){
-                res[k++] = num;
-            }
-            map[num]++;
+            actual^=num;
+        }
+        for(int num = 0;num<n;num++){
+            expected^=num;
         }
 
-        return res;
+        // Show the difference bit that not common in both numbers
+        int diff = (actual ^ expected) & -(actual ^ expected);
+
+        int a = 0, b=0;
+        for(int num: nums){
+            // Accumulate all the digits again.
+            if((num & diff) == 0) a^=num;
+            else b^=num;
+        }
+        for(int num = 0;num<n;num++){
+            // Remove repeating numbers.
+            if((num & diff) == 0) a^=num;
+            else b^=num;
+        }
+        return new int[]{a,b};
+        
     }
 }
