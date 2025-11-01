ce234afcf008a0ab3af02223cf5deabd3bb4d1b0
Mohammed Hashim <55325133+mohammedhashim790@users.noreply.github.com>
2025-10-31 23:12:48 -0300

    Time: 1 ms (55.57%), Space: 42.6 MB (16.56%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.java
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.java
@@ -12,12 +12,18 @@ class Solution {
     }
 
     public String findDifferentBinaryString(String[] nums) {
-        genBinaries(new String(), nums[0].length());
-        // System.out.println(results);
-        for(String res: nums){
-            results.remove(res);
+        // genBinaries(new String(), nums[0].length());
+        // // System.out.println(results);
+        // for(String res: nums){
+        //     results.remove(res);
+        // }
+        // return results.get(0);
+        StringBuilder res = new StringBuilder();
+        for(int index = 0;index<nums.length;index++){
+            Character ch = nums[index].charAt(index);
+            res.append(ch == '0'? '1' : '0');
         }
-        // System.out.println(results);
-        return results.get(0);
+        return res.toString();
+
     }
 }
