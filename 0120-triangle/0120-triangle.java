f9bffdbc392d7f9f332279170b6bc927fb7ee82a
Mohammed Hashim <55325133+mohammedhashim790@users.noreply.github.com>
2025-11-02 07:39:00 -0400

    Time: 10 ms (5.89%), Space: 46.6 MB (9.42%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,13 +1,28 @@
 class Solution {
-    public int minimumTotal(List<List<Integer>> triangle) {
-        int sum = 0;
-        for(List<Integer> res: triangle){
-            int min = Integer.MAX_VALUE;
-            for(Integer num: res){
-                min = Math.min(num, min);
+    public int minimumTotal(List<List<Integer>> val) {
+        int prev = 0;
+
+        for(int row = val.size()-2;row>=0;row--){
+            for(int col = 0;col < val.get(row).size();col++){
+                int currVal = val.get(row).get(col);
+                val.get(row).set(col, 
+                currVal + 
+                    Math.min(val.get(row+1).get(col), val.get(row+1).get(col+1)
+                ));
             }
-            sum+=min;
         }
-        return sum;
+
+        System.out.println(val.get(0).get(0));
+
+
+        // for(int i = 1;i < val.size(); i++){
+        //     List<Integer> row = val.get(i);
+        //     System.out.println(prev + " " + row.get(prev) + " " + row.get(prev+1) + " Min : "+Math.min(row.get(prev), row.get(prev+1)));
+        //     if(row.get(prev) > row.get(prev+1)){
+        //         prev = prev+1;
+        //     }
+        //     sum+=row.get(prev);
+        // }
+        return val.get(0).get(0);
     }
 }
