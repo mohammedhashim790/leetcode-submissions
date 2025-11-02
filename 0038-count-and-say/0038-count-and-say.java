0a68cf8fad922a671c73ba7d3108ccdfd2a4c765
Mohammed Hashim <55325133+mohammedhashim790@users.noreply.github.com>
2025-11-02 08:33:42 -0400

    Time: 18 ms (7.7%), Space: 46.3 MB (6.29%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -1,13 +1,10 @@
 class Solution {
-    final static HashMap<Integer,String> map = new HashMap<Integer, String>();
+
     static{
-        for(int i = 15 ;i<=30;i++) countAndSay(i);
+        for(int i = 1;i<=30;i++) countAndSay(i);
     }
     public static String countAndSay(int n) {
 
-        if(map.containsKey(n)){
-            return map.get(n);
-        }
         StringBuilder res = new StringBuilder("1");
         for(int i = 1;i<n;i++){
             int length = res.length()-1;
@@ -27,7 +24,7 @@ class Solution {
             builder.append(counter + "" + res.charAt(iter));
             res = new StringBuilder(builder);
         }
-        map.put(n, res.toString());
+
         return res.toString();
     }
 }
