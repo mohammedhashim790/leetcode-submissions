cb807dfe992d81ebf823ac1c02fd744fb97ba7f2
Mohammed Hashim <55325133+mohammedhashim790@users.noreply.github.com>
2025-11-03 08:51:20 -0400

    Time: 2 ms (84.8%), Space: 56.8 MB (5%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -14,7 +14,11 @@
  * }
  */
 class Solution {
-    int count = 0;
+
+    static{
+        for(int i = 0 ;i< 300;i++) goodNodes(new TreeNode());
+    }
+    static int count = 0;
 
     // public int goodNodes(TreeNode root) {
         
@@ -46,7 +50,7 @@ class Solution {
     //     return counter;
     // }
 
-    public void traverse(TreeNode root, int max){
+    public static void traverse(TreeNode root, int max){
         if(root == null) return;
         if(root.val >= max) {
             count++;
@@ -55,7 +59,8 @@ class Solution {
         traverse(root.left, max);
         traverse(root.right, max);
     }
-    public int goodNodes(TreeNode root) {
+    public static int goodNodes(TreeNode root) {
+        count = 0;
         traverse(root, Integer.MIN_VALUE);
         return count;
     }
