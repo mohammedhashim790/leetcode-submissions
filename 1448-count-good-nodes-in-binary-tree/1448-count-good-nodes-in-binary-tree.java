6dd91ce313db1faabdb035e57c8217ed26586e43
Mohammed Hashim <55325133+mohammedhashim790@users.noreply.github.com>
2025-11-03 08:49:02 -0400

    Time: 2 ms (84.8%), Space: 57 MB (5%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -14,49 +14,49 @@
  * }
  */
 class Solution {
-    
+    int count = 0;
 
-    public int goodNodes(TreeNode root) {
-        
-        Queue<Object[]> q = new LinkedList<Object[]>();
-        // List<Integer> nums = new ArrayList<Integer>();
-
-        int counter = 0;
-
-        q.offer(new Object[]{root, Integer.MIN_VALUE});
-
-
-        while(!q.isEmpty()){
-            for(int i =0 ; i<q.size();i++){
-                Object[] obj = q.poll();
-                int max = (int) obj[1];
-                TreeNode node = (TreeNode) obj[0];
-
-                if(node.val >= max){
-                    // nums.add(node.val);
-                    counter++;
-                    max = node.val;
-                }
-                if(node.left!=null) q.offer(new Object[]{node.left,max});
-                if(node.right!=null) q.offer(new Object[]{node.right,max});
-
-            }
-        }
-
-        return counter;
-    }
-
-    // public void traverse(TreeNode root, int max){
-    //     if(root == null) return;
-    //     if(root.val >= max) {
-    //         count++;
-    //         max = root.val;
-    //     }
-    //     traverse(root.left, max);
-    //     traverse(root.right, max);
-    // }
     // public int goodNodes(TreeNode root) {
-    //     traverse(root, Integer.MIN_VALUE);
-    //     return count;
+        
+    //     Queue<Object[]> q = new LinkedList<Object[]>();
+    //     // List<Integer> nums = new ArrayList<Integer>();
+
+    //     int counter = 0;
+
+    //     q.offer(new Object[]{root, Integer.MIN_VALUE});
+
+
+    //     while(!q.isEmpty()){
+    //         for(int i =0 ; i<q.size();i++){
+    //             Object[] obj = q.poll();
+    //             int max = (int) obj[1];
+    //             TreeNode node = (TreeNode) obj[0];
+
+    //             if(node.val >= max){
+    //                 // nums.add(node.val);
+    //                 counter++;
+    //                 max = node.val;
+    //             }
+    //             if(node.left!=null) q.offer(new Object[]{node.left,max});
+    //             if(node.right!=null) q.offer(new Object[]{node.right,max});
+
+    //         }
+    //     }
+
+    //     return counter;
     // }
+
+    public void traverse(TreeNode root, int max){
+        if(root == null) return;
+        if(root.val >= max) {
+            count++;
+            max = root.val;
+        }
+        traverse(root.left, max);
+        traverse(root.right, max);
+    }
+    public int goodNodes(TreeNode root) {
+        traverse(root, Integer.MIN_VALUE);
+        return count;
+    }
 }
