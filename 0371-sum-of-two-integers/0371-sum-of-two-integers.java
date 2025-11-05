54d9df7e79a81dbe450621ce1bb2e88316e974ab
Mohammed Hashim <55325133+mohammedhashim790@users.noreply.github.com>
2025-11-05 11:18:28 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.java
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.java
@@ -1,10 +1,30 @@
 class Solution {
-    public int getSum(int a, int b) {
-        while(b!=0){
-            int carry = (a & b) << 1;
-            a^=b;
-            b = carry;
+
+    public int getSum(int a, int b){
+        int carry = 0;
+        int res = 0;
+        for(int i = 0 ;i < 32;i++){
+            int aBit = (a >> i) & 1;
+            int bBit = (b >> i) & 1;
+            int bit = aBit ^ bBit ^ carry;
+            carry = ((a >> i) & (b >> i) & 1) == 1?2:0;
+            // System.out.println("Bit : " + bit);
+            if(bit!=0){
+                res |= ( 1 << i);
+            }
         }
-        return a;
+        // System.out.println(res);
+        // System.out.println(Integer.toBinaryString(res));
+        return res;
+
     }
+
+    // public int getSum(int a, int b) {
+    //     while(b!=0){
+    //         int carry = (a & b) << 1;
+    //         a^=b;
+    //         b = carry;
+    //     }
+    //     return a;
+    // }
 }
