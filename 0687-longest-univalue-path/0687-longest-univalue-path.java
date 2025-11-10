1a8a55348753ff4dce847cf6cba24cde6deaff02
Mohammed Hashim <55325133+mohammedhashim790@users.noreply.github.com>
2025-11-10 14:51:53 -0400

    Time: 2 ms (96.37%), Space: 50.2 MB (7.73%) - LeetHub

--- 0687-longest-univalue-path/0687-longest-univalue-path.java
+++ 0687-longest-univalue-path/0687-longest-univalue-path.java
@@ -16,27 +16,39 @@
 class Solution {
 
     int max = 0;
-    public void traverse(TreeNode root, int counter){
+    // public void traverse(TreeNode root, int counter){
 
-        if(root == null) return;
+    //     if(root == null) return;
 
-        int left = (root.left!=null && root.left.val == root.val)? counter+1:0;
+    //     int left = (root.left!=null && root.left.val == root.val)? counter+1:0;
 
-        int right = (root.right!=null && root.right.val == root.val)? counter+1:0;
-        if(left!=0 && right!=0){
-            max = Math.max(left+right, max);
-        }else if(max < left || max < right){
-            max = Math.max(left, right);
-        }
+    //     int right = (root.right!=null && root.right.val == root.val)? counter+1:0;
+    //     if(left!=0 && right!=0){
+    //         max = Math.max(left+right, max);
+    //     }else if(max < left || max < right){
+    //         max = Math.max(left, right);
+    //     }
     
-        System.out.println("Val : " + root.val + " Left : " + left + " Right : " + right + " Max : " + max);
+    //     System.out.println("Val : " + root.val + " Left : " + left + " Right : " + right + " Max : " + max);
 
-        traverse(root.left, left);
-        traverse(root.right, right);
+    //     traverse(root.left, left);
+    //     traverse(root.right, right);
 
+    // }
+
+    public int traverse(TreeNode root, int parent){
+        if(root == null) return 0;
+
+        int left = traverse(root.left, root.val);
+        int right = traverse(root.right, root.val);
+
+        max = Math.max(max, left+right);
+        if(root.val == parent) return Math.max(left,right) + 1;
+
+        return 0;
     }
     public int longestUnivaluePath(TreeNode root) {
-        traverse(root,0);
+        traverse(root, -1);
         return max;
     }
 }
