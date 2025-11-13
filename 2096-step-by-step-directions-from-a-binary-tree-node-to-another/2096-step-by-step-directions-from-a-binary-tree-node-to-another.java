f984d0398aeab37f819634c254008ac96a356f34
Mohammed Hashim <55325133+mohammedhashim790@users.noreply.github.com>
2025-11-13 12:36:02 -0400

    Time: 15 ms (99.9%), Space: 93.2 MB (13.93%) - LeetHub

---
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public boolean find(TreeNode root, int val, StringBuilder str){
+        if(root.val == val) return true;
+
+        if(root.left!=null && find(root.left, val, str)){
+            str.append("L");
+        }
+        else if(root.right!=null && find(root.right, val, str)){
+            str.append("R");
+        }
+
+        return str.length() > 0;
+    }
+    public String getDirections(TreeNode root, int p, int q) {
+        StringBuilder left = new StringBuilder();
+        StringBuilder right = new StringBuilder();
+        find(root, p, left);
+        find(root, q, right);
+        int i = 0, max_i = Math.min(left.length(), right.length());
+        while (i < max_i && left.charAt(left.length() - i - 1) == right.charAt(right.length() - i - 1))
+            ++i;
+        return "U".repeat(left.length() - i) + right.reverse().toString().substring(i);
+    }
+}
