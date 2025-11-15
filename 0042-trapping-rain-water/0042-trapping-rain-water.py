1bbf5d67a1636d60db2251ee7af55d4e99b917a7
Mohammed Hashim <55325133+mohammedhashim790@users.noreply.github.com>
2025-11-15 13:14:17 -0400

    Time: 15 ms (52.57%), Space: 19.3 MB (69.32%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,20 +1,35 @@
 class Solution:
     def trap(self, h: List[int]) -> int:
-        l = len(h)
-        leftMax, rightMax = [0], [0] * (l)
+        size = len(h)
         res = 0
+        l, r = 0, size-1
+        lm, rm = h[l], h[r]
 
-        for i in range(1,l):
-            leftMax.append(max(leftMax[i-1], h[i-1]))
+        while l < r:
+            if lm < rm:
+                l+=1
+                lm = max(lm, h[l])
+                res+= lm - h[l]
+            else:
+                r-=1
+                rm = max(rm, h[r])
+                res+= rm - h[r]
 
-        for i in range(l-2, -1, -1):
-            rightMax[i] = max(rightMax[i+1], h[i+1])
 
-        for i in range(l):
-            m = min(leftMax[i], rightMax[i]) - h[i]
+        # leftMax, rightMax = [0], [0] * (l)
+        # res = 0
 
-            if m>=0: 
-                res+=m
+        # for i in range(1,l):
+        #     leftMax.append(max(leftMax[i-1], h[i-1]))
+
+        # for i in range(l-2, -1, -1):
+        #     rightMax[i] = max(rightMax[i+1], h[i+1])
+
+        # for i in range(l):
+        #     m = min(leftMax[i], rightMax[i]) - h[i]
+
+        #     if m>=0: 
+        #         res+=m
 
     
         # print(leftMax)
