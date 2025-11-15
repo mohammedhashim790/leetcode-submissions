6844db6850b753cfd52d62670fe26a78a423bd6c
Mohammed Hashim <55325133+mohammedhashim790@users.noreply.github.com>
2025-11-15 10:21:58 -0400

    Time: 383 ms (97.17%), Space: 20.8 MB (20.57%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -8,18 +8,26 @@ class Solution:
         d = {}
 
         while i < l:
+            if nums[i] > 0:
+                break
+            if i > 0 and nums[i] == nums[i-1]:
+                i+=1
+                continue
             target = nums[i] * -1
             j, k = i+1, l
             while j < k:
                 t = nums[j] + nums[k]
                 if t == target:
                     # this is the answer
-                    m = str([nums[i], nums[j], nums[k]])
-                    if m not in d.keys():
-                        d[m] = 1
-                        res.append([nums[i], nums[j], nums[k]])
+                    # m = str([nums[i], nums[j], nums[k]])
+                    # if m not in d.keys():
+                    #     d[m] = 1
+                    res.append([nums[i], nums[j], nums[k]])
                     j+=1
                     k-=1
+
+                    while j<k and nums[j] == nums[j-1]:
+                        j+=1
 
                 elif t<target:
                     j+=1
