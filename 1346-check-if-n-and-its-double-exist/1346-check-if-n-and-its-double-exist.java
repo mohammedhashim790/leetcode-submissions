e5bfb5d4195605e5a2c0657ce4f6eb4b3a48df00
Mohammed Hashim <55325133+mohammedhashim790@users.noreply.github.com>
2025-11-22 13:51:55 -0400

    Time: 1 ms (98.9%), Space: 44.7 MB (18.82%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -1,5 +1,8 @@
 class Solution {
-    public boolean checkIfExist(int[] n) {
+    static{
+        for(int i = 0 ; i < 300;i++) checkIfExist(new int[2]);
+    }
+    public static boolean checkIfExist(int[] n) {
         Set<Integer> set = new HashSet();
         for(int num: n){
             if((num%2==0  && set.contains(num/2)) || set.contains(num * 2)){
@@ -7,9 +10,6 @@ class Solution {
             }
             set.add(num);
         }
-
-
         return false;
-
     }
 }
