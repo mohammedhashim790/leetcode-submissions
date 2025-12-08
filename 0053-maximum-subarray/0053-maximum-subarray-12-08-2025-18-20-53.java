5260cb23b424bf001e3ce8582a4c93fd3fdec01b
Mohammed Hashim <55325133+mohammedhashim790@users.noreply.github.com>
2025-12-08 18:20:54 -0400

    Time: 1 ms (99.74%), Space: 77.2 MB (25.21%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray-12-08-2025-18-20-53.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        
+        int sum = nums[0];
+        int max = sum;
+        for(int i = 1 ; i < nums.length;i++){
+            if(sum < 0) sum = 0;
+            sum+=nums[i];
+            max = Math.max(max, sum);
+        }
+
+        return max;
+    }
+}
