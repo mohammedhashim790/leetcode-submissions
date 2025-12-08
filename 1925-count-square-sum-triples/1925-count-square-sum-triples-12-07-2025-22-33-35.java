249f31a194eb13f83f077f3ccbb94185c06adb52
Mohammed Hashim <55325133+mohammedhashim790@users.noreply.github.com>
2025-12-07 22:33:35 -0400

    Time: 5 ms (95.82%), Space: 42.1 MB (37.58%) - LeetHub

---
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples-12-07-2025-22-33-35.java
@@ -0,0 +1,35 @@
+class Solution {
+    static HashMap<Integer, Integer> map = new HashMap<>();
+
+    static boolean isPerfectSquare(int x){
+        
+        int odd = 1;
+        while(x > 0){
+            x-=odd;
+            odd+=2;
+        }
+        return x == 0 ;
+    }
+
+    static {
+        int n = 250;
+        for(int i = 1 ;i < n;i++){
+            for(int j = i+1; j < n;j++){
+                int num = (i * i) + (j * j);
+                if(isPerfectSquare(num)){
+                    map.put(num, map.getOrDefault(num, 0 ) + 2);
+                }
+                // dead condition, but still passes
+                if(num > (n*n)) break;
+            }
+        }
+    }
+    public static int countTriples(int n) {
+        int s = 0;
+        for(int i = 1; i * i <= n * n ; i++){
+            // System.out.println(i * i + " " + map.getOrDefault(i * i,0));
+            s+=map.getOrDefault(i * i,0);
+        }
+        return s;
+    }
+}
